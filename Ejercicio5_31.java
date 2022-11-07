3ad060629880d98ddc6c17bdd96787ca126c5037
DanielJoBar <jodar.barbero.daniel@gmail.com>
2022-11-07 22:53:13 +0100

    done

---
+++ Ejercicio5_31.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ejercicio5_31 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int asterisco;
+        System.out.print("Introduzca la altura de la L: ");
+        int L = sc.nextInt();
+        for(int i = 1 ; i <= L ; i++){
+        System.out.println("*");
+        if(i==L){
+            for(int contAsterisco = 1;contAsterisco<=L;contAsterisco++){
+                System.out.print("*");
+            }
+        }
+        }
+            sc.close();
+    }
+}
---
+++ Ejercicio5_32.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Ejercicio5_32 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca un entero: ");
+        long n= sc.nextInt();
+        long mod;
+        long suma=0;
+        String pares ="";
+        long volteado=0;
+        long aux = n;
+        while(aux>0){
+        volteado=volteado*10+aux%10;
+        aux/=10;
+        }
+        while(volteado>0){
+            mod=volteado%10;
+            if(mod%2==0){
+            pares=pares+mod+" ";
+            suma=mod+suma;
+            }
+            volteado/=10;
+        }
+        System.out.println("Los numeros pares son: "+pares);
+        System.out.println("La suma de los pares es de : "+suma);
+        sc.close();
+    }
+}
